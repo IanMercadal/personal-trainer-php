ad4649fbe7ded78688225cd11dc783eabea8637e
Ian Mercadal <imercadal@cifpfbmoll.eu>
2023-01-13 11:48:38 +0100

    feat: :art: Creando header y footer
    Estilos y html

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,1 +1,35 @@
-<h2>Footer</h2>
+<footer class="footer">
+    <div class="div-logo">
+        <img class="nav-logo" src="<?= base_url ?>assets/logo.svg">
+        <h3>Gym</h3>
+    </div>
+
+    <div class="options">
+        <a class="nav-link" href="#">Inicio</a>
+        <a class="nav-link" href="#">Tarifas</a>
+        <a class="nav-link" href="#">Nosotros</a>
+    </div>
+
+    <div class="icons">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#399F2E" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-twitter" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#399F2E" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-whatsapp" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#399F2E" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+            <path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
+        </svg>
+    </div>
+
+    <p>2023 &copy; Todos los derechos reservados</p>
+</footer>
+
+</body>
+</html>
--- views/layout/header.php
+++ views/layout/header.php
@@ -4,14 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+    
+    <link rel="icon" type="image/ico" href="<?= base_url ?>assets/logo.svg">
+
     <link rel="stylesheet" href="<?=base_url?>assets/css/styles.css">
     <title>Gym</title>
 </head>
 <body>
-    
-</body>
-</html>
 
-<h2>Header</h2>
+<header>
+    <nav>
+        <div class="div-logo">
+            <img class="nav-logo" src="<?= base_url ?>assets/logo.svg">
+            <h3>Gym</h3>
+        </div>
+
+        <div class="options">
+            <a class="nav-link" href="#">Inicio</a>
+            <a class="nav-link" href="#">Tarifas</a>
+            <a class="nav-link" href="#">Nosotros</a>
+        </div>
+
+        <div>
+            <a class="login" href="#">Iniciar Sesión</a>
+        </div>
+    </nav>
+
+    <?php if ($_SERVER['REQUEST_URI'] == "/") : ?>
+        <div class="banner"></div>
+    <?php endif; ?>
+</header>
+
