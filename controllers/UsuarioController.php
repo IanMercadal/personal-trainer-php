4dda8c62e4a0dc66e70270f6fbb9c9c028901618
Ian Mercadal <imercadal@cifpfbmoll.eu>
2023-01-13 17:13:50 +0100

    feat: :art: Lista de usuarios creada
    Creando lista para admin

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -1,18 +1,20 @@
 <?php 
 
 class usuarioController {
-    public function index() {
-        echo "<h1>Perfil</h1>";
-    }
     public function login() {
         // Renderizar Vista
         require_once "views/login.php";
     }
-    public function autenticarse() {
-        header("Location:" . base_url . "usuario/perfil");
-    }
     public function perfil() {
         require_once "views/user/index.php";
+    }
+    public function admin() {
+        require_once "views/admin/users/index.php";
+    }
+
+    // Métodos
+    public function autenticarse() {
+        header("Location:" . base_url . "usuario/perfil");
     }
 }
 ?>
---
+++ views/admin/users/index.php
@@ -0,0 +1,94 @@
+<section class="contenedor">
+    <h2 class="text-center">Usuarios</h2>
+
+    <div class="filtros-usuario">
+        <form class="form-filtros">
+            <p>Campo</p>
+            <input type="text" name="campo" placeholder="buscar">
+            <select>
+                <option selected>-- Seleccionar --</option>
+                <option>ID</option>
+                <option>Nombre</option>
+                <option>Correo</option>
+                <option>Teléfono</option>
+                <option>Tarifa</option>
+            </select>
+            <a class="btn-editar" href="#">Asc</a>
+            <a class="btn-eliminar" href="#">Desc</a>
+        </form>
+    </div>
+    <div class="lista-usuarios">
+        <div class="lista-usuario">
+            <p><b>1.</b></p>
+            <p>John Doe</p>
+            <p>correo@corre.es</p>
+            <p>11111111</p>
+            <p>Tarifa Media</p>
+            <a class="btn-editar" href="#">Editar</a>
+            <a class="btn-eliminar" href="#">Eliminar</a>
+        </div>
+        <div class="lista-usuario">
+            <p><b>1.</b></p>
+            <p>John Doe</p>
+            <p>correo@corre.es</p>
+            <p>11111111</p>
+            <p>Tarifa Media</p>
+            <a class="btn-editar" href="#">Editar</a>
+            <a class="btn-eliminar" href="#">Eliminar</a>
+        </div>
+        <div class="lista-usuario">
+            <p><b>1.</b></p>
+            <p>John Doe</p>
+            <p>correo@corre.es</p>
+            <p>11111111</p>
+            <p>Tarifa Media</p>
+            <a class="btn-editar" href="#">Editar</a>
+            <a class="btn-eliminar" href="#">Eliminar</a>
+        </div>
+        <div class="lista-usuario">
+            <p><b>1.</b></p>
+            <p>John Doe</p>
+            <p>correo@corre.es</p>
+            <p>11111111</p>
+            <p>Tarifa Media</p>
+            <a class="btn-editar" href="#">Editar</a>
+            <a class="btn-eliminar" href="#">Eliminar</a>
+        </div>
+        <div class="lista-usuario">
+            <p><b>1.</b></p>
+            <p>John Doe</p>
+            <p>correo@corre.es</p>
+            <p>11111111</p>
+            <p>Tarifa Media</p>
+            <a class="btn-editar" href="#">Editar</a>
+            <a class="btn-eliminar" href="#">Eliminar</a>
+        </div>
+        <div class="lista-usuario">
+            <p><b>1.</b></p>
+            <p>John Doe</p>
+            <p>correo@corre.es</p>
+            <p>11111111</p>
+            <p>Tarifa Media</p>
+            <a class="btn-editar" href="#">Editar</a>
+            <a class="btn-eliminar" href="#">Eliminar</a>
+        </div>
+        <div class="lista-usuario">
+            <p><b>1.</b></p>
+            <p>John Doe</p>
+            <p>correo@corre.es</p>
+            <p>11111111</p>
+            <p>Tarifa Media</p>
+            <a class="btn-editar" href="#">Editar</a>
+            <a class="btn-eliminar" href="#">Eliminar</a>
+        </div>
+        <div class="lista-usuario">
+            <p><b>1.</b></p>
+            <p>John Doe</p>
+            <p>correo@corre.es</p>
+            <p>11111111</p>
+            <p>Tarifa Media</p>
+            <a class="btn-editar" href="#">Editar</a>
+            <a class="btn-eliminar" href="#">Eliminar</a>
+        </div>
+    </div>
+</section>
